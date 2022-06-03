59fcc25d4a7d4239eff7268985667b78b4642474
BorodinAG <borodin_ag@bk.ru>
2022-06-03 21:00:22 +0300

    1

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    public static Connection getConnection() {
+        String url = "jdbc:mysql://localhost:3306/first_base?serverTimezone=Europe/Moscow&useSSL=false";
+        String name = "user";
+        String password = "123";
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection(url, name, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
 }
